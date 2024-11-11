e16ca299137f2864262416a483059fc2444981db
Hari Maulana <harimaulana.xyz@gmail.com>
2024-11-11 23:10:02 +0700

    fix toggle button animation

--- src/components/navbar/SIdebar/toggleButton/ToggleButton.jsx
+++ src/components/navbar/SIdebar/toggleButton/ToggleButton.jsx
@@ -1,6 +1,7 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 
-export const ToggleButton = ({ setOpen }) => {
+export const ToggleButton = ({ setOpen, open }) => {
   return (
     <button onClick={() => setOpen((prev) => !prev)}>
       <svg width="23" height="23" viewBox="0 0 23 23">
@@ -8,6 +9,9 @@ export const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
+          d="M 2 2.5 L 20 2.5"
+          initial="closed"
+          animate={open ? "open" : "closed"}
           variants={{
             closed: { d: "M 2 2.5 L 20 2.5" },
             open: { d: "M 3 16.5 L 17 2.5" },
@@ -18,6 +22,8 @@ export const ToggleButton = ({ setOpen }) => {
           stroke="black"
           strokeLinecap="round"
           d="M 2 9.423 L 20 9.423"
+          initial="closed"
+          animate={open ? "open" : "closed"}
           variants={{
             closed: { opacity: 1 },
             open: { opacity: 0 },
@@ -27,6 +33,9 @@ export const ToggleButton = ({ setOpen }) => {
           strokeWidth="3"
           stroke="black"
           strokeLinecap="round"
+          d="M 2 16.346 L 20 16.346"
+          initial="closed"
+          animate={open ? "open" : "closed"}
           variants={{
             closed: { d: "M 2 16.346 L 20 16.346" },
             open: { d: "M 3 2.5 L 17 16.346" },
