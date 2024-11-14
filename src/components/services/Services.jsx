131d4daeec1454359773b7ccef5a352840de85b6
Hari Maulana <harimaulana.xyz@gmail.com>
2024-11-14 22:51:57 +0700

    add mobile responsive style

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -31,7 +31,8 @@ export const Services = () => {
       //   animate="animate"
       // whileInView="animate"
       ref={ref}
-      animate={isInView && "animate"}
+      // animate={isInView && "animate"}
+      animate={"animate"}
     >
       <motion.div className="textContainer" variants={variants}>
         <p>
